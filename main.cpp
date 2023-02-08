a169733fb0f2523bcd2fea1301a4708a7adf6d38
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2023-02-08 10:22:07 +0100

    open qtbridge file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <QtGui>
 #include <QtQml>
 #include <QtQuick>
+#include <QtGui/private/qzipreader_p.h>
 
 int main(int argc, char **argv){
     QGuiApplication app(argc, argv);
-//    QQmlEngine engine;
-//    QQmlComponent component(&engine);
-//    component.loadUrl(QUrl("qrc:///main.qml"));
-//    QWindow *w = qobject_cast<QWindow *>(component.create());
-//    w->show();
-    QQmlApplicationEngine engine(QUrl("qrc:///main.qml"));
+
+    if (argc < 2) {
+        qDebug() << "Usage: appname <--images-only> [.qtbridge]";
+        return 0;
+    }
+
+    const QString fileName(argv[1]);
+    qDebug() << "filename:" << fileName;
+
+    // QQmlApplicationEngine engine(QUrl("qrc:///main.qml"));
     return app.exec();
 }
 
