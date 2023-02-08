629a8b9dd9fb5d1b825955ac516ea9e5298b4069
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2023-02-08 14:09:19 +0100

    use vscode build setup

--- main.cpp
+++ main.cpp
@@ -12,9 +12,14 @@ int main(int argc, char **argv){
     }
 
     const QString fileName(argv[1]);
-    qDebug() << "filename:" << fileName;
+    const QFileInfo fileInfo(fileName);
+    const QString suffix = fileInfo.suffix();
+
+    // const QFile file(QString(argv[1]));
+   
+    // qDebug() << "filename:" << file.fileName();
 
     // QQmlApplicationEngine engine(QUrl("qrc:///main.qml"));
-    return app.exec();
+    // return app.exec();
+    return 0;
 }
-
