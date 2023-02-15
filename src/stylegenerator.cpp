3b69fa87b26ac971e5334a666d4649c15f9d3232
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2023-02-15 13:12:21 +0100

    implement generateImage()

--- src/stylegenerator.cpp
+++ src/stylegenerator.cpp
@@ -24,7 +24,16 @@ void setTargetPath(const QString path)
         throw std::runtime_error("Could not create target path: " + path.toStdString());
 }
 
-QJsonObject getTemplateObject(const QString &templateName, const QJsonDocument &doc)
+void copyImage(const QString srcName, const QString targetName)
+{
+    const QString srcFilePath = resourcePath + '/' + srcName;
+    QFile srcFile = QFile(srcFilePath);
+    if (!srcFile.exists())
+        throw std::runtime_error("File doesn't exist: " + srcFilePath.toStdString());
+    srcFile.copy(styleDir + "/" + targetName);
+}
+
+QJsonObject getTemplateRootObject(const QString &templateName, const QJsonDocument &doc)
 {
     // TODO: When using real-life data, 'artboardSets' will probably not
     // be on the root node, so this function will need to be adjusted!
@@ -43,20 +52,17 @@ QString getImageFileName(const QJsonObject &templateObject, const QString &state
     return getValue("assetPath").toString();
 }
 
-void copyImage(const QString srcName, const QString targetName)
+void generateImage(const QString &targetFileName, const QString &state, const QJsonObject templateObject)
 {
-    const QString srcFilePath = resourcePath + '/' + srcName;
-    QFile srcFile = QFile(srcFilePath);
-    if (!srcFile.exists())
-        throw std::runtime_error("File doesn't exist: " + srcFilePath.toStdString());
-    srcFile.copy(styleDir + "/" + targetName);
+    const QString srcName = getImageFileName(templateObject, QString("state=") + state);
+    copyImage(srcName, targetFileName + ".svg");
 }
 
 void generateButton(const QJsonDocument &doc)
 {
-    const QJsonObject buttonTemplate = getTemplateObject("ButtonTemplate", doc);
-    const QString bgFileName = getImageFileName(buttonTemplate, "state=idle");
-    copyImage(bgFileName, "buttonBackground.svg");
+    const QJsonObject buttonTemplate = getTemplateRootObject("ButtonTemplate", doc);
+    generateImage("button-background", "idle", buttonTemplate);
+    generateImage("button-background-hovered", "hovered", buttonTemplate);
 }
 
 } // namespace
